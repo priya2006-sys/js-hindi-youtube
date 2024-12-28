4c724ca97fb4215c3b2dbc3e03dc92e4809f1e01
Priya Tiwari <justpriya2007@gmail.com>
2024-12-28 15:50:34 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountId= 144533
+let accountEmail ="priya@google.com"
+var accountPassword ="12345"
+accountCity="jaipur"
+
+//accountId =2
+/*
+prefer not to use var 
+*/
+
+
+accountEmail ="hc@hc.com"
+accountPassword="212121"
+accountCity ="mumbai"
+
+console.log(accountId)
+
+console.table([ accountId,accountEmail,accountPassword,accountCity])
+
+
+
+
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict";
+/*treat all the js code as newer version
+ once you have return "use strict" like this your whole code will run as newer version */
+
+
+
+
+
+
+ //datatypes
+ //number => 2to power 53
+ //bigint
+ //string => true/false
+ //null => standalone value
+ //undefined =>
+ //symbol =>umique
+ 
+ 
+
+ //object
+
+ console.log(typeof"Priya");
+
+ console.log(typeof"null"); //null is an object datatype
