f2b9f184d90583566a6fbc6f48a4ec5822c57ba0
Priya Tiwari <justpriya2007@gmail.com>
2025-01-02 15:55:37 +0000

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,34 @@
+//#\ Primitive
+
+/* 7 types : string, number , boolean, null , undefined
+symbol, BigInt */
+
+// const score = 100
+// const scorevalue =100.3
+// const isLoggedIn = false
+// cont outsideTemp= null
+// let useremail;
+
+
+cont id= symbol('123')
+const anotherId = symbol('123')
+
+// console.log(id === anotherId);
+
+
+// reference(non primitive)
+
+// array , objects , functions
+
+const heroes =["shaktiman" , "naagraj" , "doga"];
+let myobj ={
+    name: "priya",
+    age: 19,
+}
+
+ const myFunction =function(){
+    console.log("hello world");   
+ }
+ console.log(typeof bigNumber);
+ 
+
