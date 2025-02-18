24893d57ec5959d7024b68e3334ff8296247479d
Priya Tiwari <justpriya2007@gmail.com>
2025-02-18 15:13:10 +0000

    array.js

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,11 @@
+const MyArr =[0,1,2,3,4,5]
+const myheroes =["Papa","Mummmy"]
+
+const MyArr2 = new array(1,2,3,4)
+console.log(MyArr[3]);
+
+ //array methods
+ MyArr =push()
+ MyArr2
+ MyArr2
+
---
+++ 02_basics/02_array.js
@@ -0,0 +1,24 @@
+const marvel_heroes =["Hulk", "IronMan", "Thor"]
+const dc_heroes=["batman", "Superman", "spiderman"]
+// marvel_heroes.push(dc)
+// console.log(marvel_heroes);
+// console.log(marvel_heroes[3][1]);
+
+// const allheroes =marvel_heroes.concat(dc)
+// console.log(allheroes);
+
+// const all_new_heroes = [...marvel_heroes, ...dc_heroes]
+// console.log(all_new_heroes);
+
+// const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7 ,[4, 5]]]
+
+// const real_another_array = another_array.flat(Infinity)
+// console.log(real_another_array);
+
+// console.log(Array.from({name: Priya}));
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
