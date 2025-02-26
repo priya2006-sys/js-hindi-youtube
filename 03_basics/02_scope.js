c11c060101da1c5eadc11e46a5b5fa963a675b0f
Priya Tiwari <justpriya2007@gmail.com>
2025-02-26 07:49:29 +0000

    scope.js

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -5,10 +5,46 @@
 if (true){
     let a = 10;
     const b = 20;    
-    console.log("Inner:", 10);
+   // console.log("Inner:", 10);
      
 }
 
- console.log(a);
+ //console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one(){
+    const username ="Priya"
+    function two(){
+        const website = "youtube"
+        //console.log(username);
+    }
+     // console.log(website);
+      two()
+}
+   one()
+
+if (true) {
+   const username ="Priya" 
+   if (username === "Priya") {
+    const website = "  youtube "
+    console.log(username + website);
+    
+   }
+   //console.log(website);
+   
+}
+//console.log(username);
+
+//+++++++++++++++++++++++++++++interesting+++++++++++++++++++++++++++++++++++++++++
+console.log(addone(5));
+function addone(num){
+   return + 1
+}
+console.log(addTwo(5));
+
+const addTwo = function(num){
+   return  num + 2
+}
+
